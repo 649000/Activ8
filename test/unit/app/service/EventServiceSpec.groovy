d20fbd1b8825be4d9cda0795016f08686180911e
Kenny <bionic_ice@hotmail.com>
2015-03-26 15:53:17 +0800

    Change1

--- test/unit/app/service/EventServiceSpec.groovy
+++ test/unit/app/service/EventServiceSpec.groovy
@@ -3,13 +3,19 @@ package app.service
 import app.event.CategoryService
 import app.event.Event
 import app.event.EventService
+import app.filter.EventFilter
 import grails.test.mixin.TestFor
+import grails.test.mixin.TestMixin
+import grails.test.runtime.FreshRuntime
 import org.junit.Assert
 import spock.lang.Specification
-
+import grails.test.mixin.support.GrailsUnitTestMixin
+import grails.test.mixin.services.ServiceUnitTestMixin
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
+@FreshRuntime
+@TestMixin(ServiceUnitTestMixin)
 @TestFor(EventService)
 class EventServiceSpec extends Specification {
 
@@ -18,28 +24,43 @@ class EventServiceSpec extends Specification {
 
     def cleanup() {
     }
-
     private void createMockEvent(){
-        Event category = new Event();
-
+        Event event = new Event();
+        event.name = "asd";
+        event.maxCapacity = 5;
     }
-    void "Test event creation"() {
+    def "grailsApplication is not null"() {
+        expect:
+        grailsApplication != null
+    }
+    def "Test_event_creation"() {
+        given:
         Event mockEvent = createMockEvent();
         EventService eventService = new EventService();
         CategoryService categoryService = new CategoryService();
-        Category defaultCategory = categoryService.getAllCategories().get(0);
-        //Create event
+        when:
+        List<Category> allCategories = categoryService.getAllCategories();
+        Category defaultCategory = allCategories.get(0);
+        //Create event 
         int eventId = eventService.createEvent(mockEvent);
-        Assert.assertTrue("Create event", true);
         //Retrieve created event
         Event retrievedEvent = eventService.getEventById(eventId);
-        Assert.assertEquals(mockEvent.getName(), retrievedEvent.getName());
-        Assert.assertTrue("Retrieve created event", true);
         //Add events to category
         eventService.addEventToCategory(defaultCategory, mockEvent);
-        Assert.assertTrue("Add event to category", true);
+        EventFilter eventFilter = new EventFilter();
+        eventFilter.categoryId = defaultCategory.categoryId;
+        def eventList = eventService.getEventsByFilter(eventFilter);
+        boolean hasFoundEvent = false;
+        for (Event event: eventList){
+            if (event.name == mockEvent.name && event.id) 
+                {hasFoundEvent = true; break;}
+        }
         //Delete created event
         eventService.deleteEvent(retrievedEvent);
-        Assert.assertTrue("Delete event", true);
+        then:
+        eventId != 0
+        mockEvent.getName() == retrievedEvent.getName()
+        retrievedEvent != null
+        hasFoundEvent == true 
     }
 }
